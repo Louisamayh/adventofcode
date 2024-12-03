53a544b13b5a46361ed8b2fc75d5a67082e8596c
Louisamay Hanrahan <louisamayhanrahan@Louisamays-MacBook-Pro.local>
2024-12-03 16:30:38 +0100

    Add Day 3 solution script and input file

---
+++ day2/day2code.py
@@ -0,0 +1,48 @@
+
+#load text file into python
+with open("day2p1input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+
+#seperate column and turn into integers
+counter = 0
+
+for i in lines:
+    linecols = i.split(" ")
+        
+    lineready = []
+
+    for i in linecols:
+        lineready.append(int(i))
+    
+    #convert lists to desending by checkin first and last integer
+
+    if lineready[0] < lineready[-1]:
+        lineready.reverse()
+    lineready
+
+    # rules met? Counter = counter + 1
+
+    #check if the numbers in the row are all desending
+    success = True
+    for i in range(len(lineready)-1):
+        if lineready[i] - lineready[i+1] < 1 or lineready[i] - lineready[i+1] > 3:
+            print('breaking because ', lineready[i] , lineready[i+1])
+            success = False
+            break
+
+    if success:
+        counter = counter + 1
+print(counter)
+
+   
+
+
+#check if the difference between the next number is more than 3
+
+#check if the difference between the next number is 0
+
+
+
+
+
